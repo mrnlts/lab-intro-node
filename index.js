b4f46efc8db218ee41f29912887a7523c19e91bc
marina <marinaaltesmagret@gmail.com>
2021-04-08 18:45:36 +0200

    Mandatory iterations

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    function compareNumbers(a, b) {
+      return a-b;
+    }
+    this.items.sort(compareNumbers);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length || pos < 0) {
+      throw new Error ('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
   sum() {}
 
